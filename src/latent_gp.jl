338910edce284de6754e681c5eb701f4b64a7db1
Sharan Yalburgi <sharanyalburgi@gmail.com>
2020-05-27 18:31:31 +0530

    Rename \Phi to lik

--- src/latent_gp.jl
+++ src/latent_gp.jl
@@ -1,18 +1,18 @@
 """
-    LatentGP(f<:GP, x<:AbstractVector, Φ)
+    LatentGP(f<:GP, x<:AbstractVector, lik)
 
  - `fx` is a `FiniteGP`.
- - `Φ` is the log likelihood function which maps input of type `Tx` to `Real`.
+ - `lik` is the log likelihood function which maps input of type `Tx` to `Real`.
     
 """
-struct LatentGP{T<:AbstractGPs.FiniteGP}
+struct LatentGP{T<:AbstractGPs.FiniteGP, S}
     fx::T
-    Φ
+    lik::S
 end
 
 function Distributions.rand(rng::AbstractRNG, lgp::LatentGP)
     f = rand(rng, lgp.fx)
-    y = rand(rng, lgp.Φ(f))
+    y = rand(rng, lgp.lik(f))
     return (f=f, y=y)
 end
 
@@ -33,5 +33,5 @@ randy(lgp::LatentGP) = rand(lgp).y
 Returns the joint log density of the gaussian process output `f` and real output `y`.
 """
 function Distributions.logpdf(lgp::LatentGP, y::NamedTuple{(:f, :y)})
-    return logpdf(lgp.fx, y.f) + logpdf(lgp.Φ(y.f), y.y)
+    return logpdf(lgp.fx, y.f) + logpdf(lgp.lik(y.f), y.y)
 end
