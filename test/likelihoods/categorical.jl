4297ff7d4a5394f02b4fcecaf2103b6ea389ce31
Sharan Yalburgi <sharanyalburgi@gmail.com>
2020-09-29 12:55:43 +0530

    Remove irrelevant comment

--- test/likelihoods/categorical.jl
+++ test/likelihoods/categorical.jl
@@ -1,7 +1,7 @@
 @testset "CategoricalLikelihood" begin
     lik = CategoricalLikelihood()
     IN_DIM = 3
-    OUT_DIM = 2 # one for the mean the other for the log-standard deviation
+    OUT_DIM = 4
     N = 10
     X = MOInput([rand(IN_DIM) for _ in 1:N], OUT_DIM)
     test_interface(lik, IndependentMOKernel(SqExponentialKernel()), X)
