d65187e1051d98eddbf7c03dc9ec5f1f38e60c30
Sharan Yalburgi <sharanyalburgi@gmail.com>
2020-10-30 20:12:34 +0530

    Reduce max line width

--- examples/PalmerPenguins.jl
+++ examples/PalmerPenguins.jl
@@ -13,11 +13,19 @@ df[!,:flipper_length_mm] = convert(Vector{Float64}, df[!,:flipper_length_mm])
 df[!,:body_mass_g] = convert(Vector{Float64}, df[!,:body_mass_g])
 first(df, 5)
 
-# The Palmer penguins dataset consists of measurements of 344 penguins from three islands in the Palmer Archipelago, Antarctica, that were collected by Dr. Kristen Gorman and the Palmer Station, Antarctica LTER (Gorman, Williams, & Fraser (2014)). The simplified version of the dataset contains at most seven measurements for each penguin, namely the species (Adelie, Chinstrap, and Gentoo), the island (Torgersen, Biscoe, and Dream), the bill length (measured in mm), the bill depth (measured in mm), the flipper length (measured in mm), the body mass (measured in g), and the sex (male and female). [Source](https://widmann.dev/blog/2020/07/palmerpenguins/)
+# The Palmer penguins dataset consists of measurements of 344 penguins from three islands 
+# in the Palmer Archipelago, Antarctica, that were collected by Dr. Kristen Gorman and 
+# the Palmer Station, Antarctica LTER (Gorman, Williams, & Fraser (2014)). The simplified 
+# version of the dataset contains at most seven measurements for each penguin, namely the 
+# species (Adelie, Chinstrap, and Gentoo), the island (Torgersen, Biscoe, and Dream), the 
+# bill length (measured in mm), the bill depth (measured in mm), the flipper length 
+# (measured in mm), the body mass (measured in g), and the sex (male and female). 
+# [Source](https://widmann.dev/blog/2020/07/palmerpenguins/)
 
 # ## Binary classification using Bernoulli likelihood
 
-# In this example we will demonstrate binary classfication on the sex of thee penguins based on their bill length & depth. flipper length and body mass.
+# In this example we will demonstrate binary classfication on the sex of thee 
+# penguins based on their bill length & depth. flipper length and body mass.
 
 unique(df[!,:sex])
 
@@ -39,7 +47,8 @@ scatter(
 
 # A visualization of three of the four input parameters.
 
-# We split the dataset in train and test. Approximately 20% of the dataset is held out as test data.
+# We split the dataset in train and test. Approximately 20% of the 
+# dataset is held out as test data.
 
 x_train, y_train = x[1:266], y[1:266]
 x_test, y_test = x[267:end], y[267:end];
@@ -62,7 +71,10 @@ end
 
 # # Elliptical Slice Sampling
 
-# We will use [elliptical slice sampling](http://proceedings.mlr.press/v9/murray10a/murray10a.pdf) to infer the kernel parameters along with the outputs. [EllipticalSliceSampling.jl](https://github.com/TuringLang/EllipticalSliceSampling.jl/) gives us a easy to use implementation of this.
+# We will use [elliptical slice sampling](http://proceedings.mlr.press/v9/murray10a/murray10a.pdf)
+# to infer the kernel parameters along with the outputs. 
+# [EllipticalSliceSampling.jl](https://github.com/TuringLang/EllipticalSliceSampling.jl/) 
+# gives us a easy to use implementation of this.
 
 prior = MvNormal(2+length(x_train), 1)
 samples = sample(ESSModel(prior, ℓ), ESS(), 1_000)
@@ -85,7 +97,9 @@ mean_params = mean(samples)
 plt = histogram(samples_mat[1:2,:]'; layout=2, labels= "", bins=10)
 vline!(plt, mean_params[1:2]'; layout=2, label="Mean", size=(500,250))
 
-# A helper function to compute the mean posterior predictions of the binary classification task. This gives the probability of a particular penguin being a female.
+# A helper function to compute the mean posterior predictions of the binary 
+# classification task. This gives the probability of a particular penguin 
+# being a female.
 
 function posterior_mean(posterior_params)
     ys = [
