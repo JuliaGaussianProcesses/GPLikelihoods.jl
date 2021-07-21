93455a48994f05c5d1b354fe7f4ff2bebbca1b64
Théo Galy-Fajou <theo.galyfajou@gmail.com>
2021-07-21 19:05:16 +0200

    Fix docstrings

--- src/links.jl
+++ src/links.jl
@@ -2,7 +2,7 @@
     AbstractLink
 
 Abstract type defining maps from R^n -> X.
-They can be applied by calling `link(x)`
+They can be applied by calling `link(x)`.
 
 A series of definitions are given in http://web.pdx.edu/~newsomj/mvclass/ho_link.pdf
 """
@@ -19,7 +19,7 @@ apply(l::ChainLink, x) = foldl((x, l) -> l(x), l.ls; init=x)
 """
     Link(f)
 
-General construction for a link with a function `f`
+General construction for a link with a function `f`.
 """
 struct Link{F} <: AbstractLink
     f::F
@@ -30,7 +30,7 @@ apply(l::Link, x) = l.f(x)
 """
     LogLink()
 
-`log` link: f:ℝ⁺->ℝ . Its inverse is the [`ExpLink`](@ref)
+`log` link, f:ℝ⁺->ℝ . Its inverse is the [`ExpLink`](@ref).
 """
 struct LogLink <: AbstractLink end
 
@@ -41,7 +41,7 @@ Base.inv(::LogLink) = ExpLink()
 """
     ExpLink()
 
-`exp` link. f:ℝ->ℝ⁺. Its inverse is the [`ExpLink`](@ref)
+`exp` link, f:ℝ->ℝ⁺. Its inverse is the [`LogLink`](@ref).
 """
 struct ExpLink <: AbstractLink end
 
@@ -52,7 +52,7 @@ Base.inv(::ExpLink) = LogLink()
 """
     InvLink()
 
-`inv` link. f:ℝ/{0}->ℝ/{0}. It is its own inverse.
+`inv` link, f:ℝ/{0}->ℝ/{0}. It is its own inverse.
 """
 struct InvLink <: AbstractLink end
 
@@ -63,8 +63,7 @@ Base.inv(::InvLink) = InvLink()
 """
     SqrtLink()
 
-`sqrt` link. f:ℝ⁺∪{0}->ℝ⁺∪{0}.
-Its inverse is the [`SquareLink`](@ref)
+`sqrt` link, f:ℝ⁺∪{0}->ℝ⁺∪{0}. Its inverse is the [`SquareLink`](@ref).
 """
 struct SqrtLink <: AbstractLink end
 
@@ -75,8 +74,7 @@ Base.inv(::SqrtLink) = SquareLink()
 """
     SquareLink()
 
-`^2` link. f:ℝ->ℝ⁺∪{0}.
-Its inverse is the [`SqrtLink`](@ref)
+`^2` link, f:ℝ->ℝ⁺∪{0}. Its inverse is the [`SqrtLink`](@ref).
 """
 struct SquareLink <: AbstractLink end
 
@@ -87,8 +85,7 @@ Base.inv(::SquareLink) = SqrtLink()
 """
     LogitLink()
 
-Determines the `logit` link, i.e `log(x/(1-x))`, f:[0,1]->ℝ
-Its inverse is the [`LogisticLink`](@ref).
+`log(x/(1-x))` link, f:[0,1]->ℝ. Its inverse is the [`LogisticLink`](@ref).
 """
 struct LogitLink <: AbstractLink end
 
@@ -99,8 +96,7 @@ Base.inv(::LogitLink) = LogisticLink()
 """
     LogisticLink()
 
-Determines the `logistic` link, i.e `exp(x)/(1+exp(-x))`. f:ℝ->[0,1]
-Its inverse is the [`Logit`](@ref).
+`exp(x)/(1+exp(-x))` link. f:ℝ->[0,1]. Its inverse is the [`Logit`](@ref).
 """
 struct LogisticLink <: AbstractLink end
 
@@ -108,6 +104,12 @@ apply(::LogisticLink, x) = logistic(x)
 
 Base.inv(::LogisticLink) = LogitLink()
 
+"""
+    ProbitLink()
+
+`ϕ⁻¹(y)` link, where `ϕ⁻¹` is the `invcdf` of a `Normal` distribution, f:[0,1]->ℝ.
+Its inverse is the [`NormalCDFLink`](@ref).
+"""
 struct ProbitLink <: AbstractLink end
 
 apply(::ProbitLink, x) = norminvcdf(x)
@@ -117,8 +119,8 @@ Base.inv(::ProbitLink) = NormalCDFLink()
 """
     NormalCDFLink()
 
-Determines the `cdf` of a normal, i.e ∫₋∞ˣ p(x')dx'. f:ℝ->[0,1]
-Its inverse is the [`Logit`](@ref).
+`ϕ(y)` link, where `ϕ` is the `cdf` of a `Normal` distribution, f:ℝ->[0,1].
+Its inverse is the [`ProbitLink`](@ref).
 """
 struct NormalCDFLink <: AbstractLink end
 
@@ -131,7 +133,9 @@ apply(::ChainLink{<:Tuple{LogLink,NormalCDFLink}}, x) = normlogcdf(x) # Speciali
 """
     SoftMaxLink()
 
-Determines the `softmax` link, i.e `exp(f)/(1+exp(-x))`. f:ℝⁿ->Sⁿ. Where Sⁿ is an n-simplex : TODO to check
+`softmax` link, i.e `f(x)ᵢ = exp(xᵢ)/∑ⱼexp(xⱼ)`.
+f:ℝⁿ->Sⁿ, where Sⁿ is an n-simplex
+It has no defined inverse
 """
 struct SoftMaxLink <: AbstractLink end
 
