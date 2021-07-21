17fdb3bdff1c2903a158e9dfa2041d302ffbec2c
Théo Galy-Fajou <theo.galyfajou@gmail.com>
2021-07-21 19:05:55 +0200

    Add inv(inv(x)) == x tests

--- test/links.jl
+++ test/links.jl
@@ -11,41 +11,49 @@
     l = LogLink()
     @test l(x) == log(x)
     @test inv(l) == ExpLink()
+    @test inv(inv(l)) == l
 
     # Exp
     l = ExpLink()
     @test l(x) == exp(x)
     @test inv(l) == LogLink()
+    @test inv(inv(l)) == l
     
     # Sqrt
     l = SqrtLink()
     @test l(x) == sqrt(x)
     @test inv(l) == SquareLink()
+    @test inv(inv(l)) == l
 
     # Square
     l = SquareLink()
     @test l(x) == x^2
     @test inv(l) == SqrtLink()
+    @test inv(inv(l)) == l
 
     # Logit
     l = LogitLink()
     @test l(x) == logit(x)
     @test inv(l) isa LogisticLink
+    @test inv(inv(l)) == l
 
     # Logistic
     l = LogisticLink()
     @test l(x) == logistic(x)
     @test inv(l) isa LogitLink
+    @test inv(inv(l)) == l
 
     # Probit
     l = ProbitLink()
     @test l(x) == norminvcdf(x)
     @test inv(l) == NormalCDFLink()
+    @test inv(inv(l)) == l
 
     # NormalCDF
     l = NormalCDFLink()
     @test l(x) == normcdf(x)
     @test inv(l) == ProbitLink()
+    @test inv(inv(l)) == l
 
     # SoftMax
     l = SoftMaxLink()
