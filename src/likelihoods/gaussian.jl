2f6b240a63586deb2f5ed9ec387d16d70f3869ab
Théo Galy-Fajou <theo.galyfajou@gmail.com>
2021-09-03 12:10:51 +0200

    Fix the definitions for the heteroscedastic likelihood

--- src/likelihoods/gaussian.jl
+++ src/likelihoods/gaussian.jl
@@ -21,19 +21,19 @@ GaussianLikelihood(σ²::Real) = GaussianLikelihood([σ²])
 
 (l::GaussianLikelihood)(f::Real) = Normal(f, sqrt(first(l.σ²)))
 
-(l::GaussianLikelihood)(fs::AbstractVector{<:Real}) = MvNormal(fs, sqrt(first(l.σ²)))
+(l::GaussianLikelihood)(fs::AbstractVector{<:Real}) = MvNormal(fs, first(l.σ²) * I)
 
 """
     HeteroscedasticGaussianLikelihood(l::AbstractLink=ExpLink())
 
 Heteroscedastic Gaussian likelihood. 
-This is a Gaussian likelihood whose mean and the log of whose variance are functions of the
-latent process.
+This is a Gaussian likelihood whose mean and variance are functions of
+latent processes.
 
 ```math
-    p(y|[f, g]) = Normal(y | f, l(g))
+    p(y|[f, g]) = Normal(y | f, sqrt(l(g)))
 ```
-On calling, this would return a normal distribution with mean `f` and std. dev. `l(g)`.
+On calling, this would return a normal distribution with mean `f` and variance `l(g)`.
 Where `l` is link going from R to R^+
 """
 struct HeteroscedasticGaussianLikelihood{Tl<:AbstractLink}
@@ -43,7 +43,7 @@ end
 HeteroscedasticGaussianLikelihood() = HeteroscedasticGaussianLikelihood(ExpLink())
 
 function (l::HeteroscedasticGaussianLikelihood)(f::AbstractVector{<:Real})
-    return Normal(f[1], l.invlink(f[2]))
+    return Normal(f[1], sqrt(l.invlink(f[2])))
 end
 
 function (l::HeteroscedasticGaussianLikelihood)(fs::AbstractVector)
