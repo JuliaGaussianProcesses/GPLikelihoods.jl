56f6dbb65ff703109ad5e075481258b6f943fa46
Théo Galy-Fajou <theo.galyfajou@gmail.com>
2021-09-29 14:44:41 +0200

    Missing change for heteroscedastic

--- src/likelihoods/gaussian.jl
+++ src/likelihoods/gaussian.jl
@@ -31,7 +31,7 @@ This is a Gaussian likelihood whose mean and variance are functions of
 latent processes.
 
 ```math
-    p(y|[f, g]) = \\mathrm{Normal}(y | f, sqrt(l(g)))
+    p(y|[f, g]) = \\operatorname{Normal}(y | f, sqrt(l(g)))
 ```
 On calling, this would return a normal distribution with mean `f` and variance `l(g)`.
 Where `l` is link going from R to R^+
