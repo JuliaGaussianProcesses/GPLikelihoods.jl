008f4ac1a065ab3185e9b0f88316112aed7547f2
Théo Galy-Fajou <theo.galyfajou@gmail.com>
2021-09-29 14:48:36 +0200

    Use math mode for list of inputs

--- src/likelihoods/categorical.jl
+++ src/likelihoods/categorical.jl
@@ -6,8 +6,8 @@ uncertainity associated with the data follows a Categorical distribution.
 ```math
     p(y|f_1, f_2, \\dots, f_{n-1}) = \\operatorname{Categorical}(y | l(f_1, f_2, \\dots, f_{n-1}, 0))
 ```
-Given an `AbstractVector` [f_1, f_2, ..., f_{n-1}], returns a `Categorical` distribution,
-with probabilities given by `l(f_1, f_2, ..., f_{n-1}, 0)`.
+Given an `AbstractVector` ``[f_1, f_2, ..., f_{n-1}]``, returns a `Categorical` distribution,
+with probabilities given by ``l(f_1, f_2, ..., f_{n-1}, 0)``.
 """
 struct CategoricalLikelihood{Tl<:AbstractLink} <: AbstractLikelihood
     invlink::Tl
