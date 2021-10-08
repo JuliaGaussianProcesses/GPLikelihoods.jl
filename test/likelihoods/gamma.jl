2b54ce27e20e4d52f86768a5f9fc595d6a020566
David Widmann <devmotion@users.noreply.github.com>
2021-10-09 00:43:10 +0200

    Test GammaLikelihood

--- test/likelihoods/gamma.jl
+++ test/likelihoods/gamma.jl
@@ -1,4 +1,10 @@
 @testset "GammaLikelihood" begin
+    for args in ((), (1.0,), (exp,), (ExpLink(),), (1.0, exp), (1.0, ExpLink()))
+        lik = GammaLikelihood(args...)
+        @test lik isa GammaLikelihood{Float64,ExpLink}
+        @test lik.α == 1
+    end
+
     lik = GammaLikelihood(1.0)
     test_interface(lik, SqExponentialKernel(), rand(10); functor_args=(:α, :invlink))
 end
