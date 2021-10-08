80ee57c17651fd1ee8a643198c5607a596ccf9b4
David Widmann <devmotion@users.noreply.github.com>
2021-10-09 00:21:31 +0200

    Add tests for PoissonLikelihood

--- test/likelihoods/poisson.jl
+++ test/likelihoods/poisson.jl
@@ -1,4 +1,8 @@
 @testset "PoissonLikelihood" begin
-    lik = PoissonLikelihood()
-    test_interface(lik, SqExponentialKernel(), rand(10))
+    @test PoissonLikelihood() isa PoissonLikelhood{ExpLink}
+    @test PoissonLikelihood(ExpLink()) isa PoissonLikelihood{ExpLink}
+
+    for lik in (PoissonLikelihood(), PoissonLikelihood(log1pexp))
+        test_interface(lik, SqExponentialKernel(), rand(10))
+    end
 end
