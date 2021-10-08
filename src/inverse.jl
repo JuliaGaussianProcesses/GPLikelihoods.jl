91a4694d3364bd65ef8b729194c0ffae995d5dae
David Widmann <devmotion@users.noreply.github.com>
2021-10-08 17:06:30 +0200

    Add docstring

--- src/inverse.jl
+++ src/inverse.jl
@@ -1,3 +1,13 @@
+"""
+    inverse(f)
+
+Return the inverse of function `f`.
+
+This is an internal function to avoid type piracies such as `inv(::typeof(exp))`. At some
+point `inv` might support standard Julia functions which would allow us to remove `inverse`.
+"""
+inverse(f)
+
 inverse(::typeof(exp)) = log
 inverse(::typeof(log)) = exp
 
