5e3aa5b0de6bbaef743b24fddbd74308336faee6
Théo Galy-Fajou <theo.galyfajou@gmail.com>
2022-01-04 13:52:48 +0100

    Tidying up
    Co-authored-by: willtebbutt <wct23@cam.ac.uk>

--- test/likelihoods/categorical.jl
+++ test/likelihoods/categorical.jl
@@ -4,9 +4,6 @@
     end
 
     lik = CategoricalLikelihood()
-    # IN_DIM = 3
     OUT_DIM = 4
-    # N = 10
-    # X = MOInput([rand(IN_DIM) for _ in 1:N], OUT_DIM)
     test_interface(lik, Categorical, OUT_DIM)
 end
--- test/likelihoods/gaussian.jl
+++ test/likelihoods/gaussian.jl
@@ -13,6 +13,5 @@ end
     IN_DIM = 3
     OUT_DIM = 2 # one for the mean the other for the log-standard deviation
     N = 10
-    # X = MOInput([rand(IN_DIM) for _ in 1:N], OUT_DIM)
     test_interface(lik, Normal, 2)
 end
