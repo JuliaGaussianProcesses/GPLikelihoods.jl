86e656d57159dc4584084e6a7a74c8eac9152fa1
Simone Surace <simone.surace@artificialy.com>
2022-01-28 15:33:35 +0100

    Modify test to reflect type parameter order

--- test/likelihoods/negativebinomial.jl
+++ test/likelihoods/negativebinomial.jl
@@ -1,8 +1,8 @@
 @testset "NegativeBinomialLikelihood" begin
     for args in
-        ((), (1.0,), (logistic,), (LogisticLink(),), (1.0, logistic), (1.0, LogisticLink()))
+        ((), (1,), (logistic,), (LogisticLink(),), (1, logistic), (1, LogisticLink()))
         lik = NegativeBinomialLikelihood(args...)
-        @test lik isa NegativeBinomialLikelihood{Float64,LogisticLink}
+        @test lik isa NegativeBinomialLikelihood{LogisticLink,Int}
         @test lik.r == 1
     end
 
