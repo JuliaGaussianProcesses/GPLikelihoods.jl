eed1a18a182bac8799d6d6afdbf84a3a71cf36f0
Simone Surace <simone.surace@artificialy.com>
2022-01-28 16:15:14 +0100

    Change tests appropriately

--- test/likelihoods/negativebinomial.jl
+++ test/likelihoods/negativebinomial.jl
@@ -1,11 +1,18 @@
 @testset "NegativeBinomialLikelihood" begin
-    for args in
-        ((), (1,), (logistic,), (LogisticLink(),), (1, logistic), (1, LogisticLink()))
-        lik = NegativeBinomialLikelihood(args...)
+    for args in ((), (logistic,), (LogisticLink(),)),
+        kwargs in ((), (; r=1))
+        lik = NegativeBinomialLikelihood(args...; kwargs...)
         @test lik isa NegativeBinomialLikelihood{LogisticLink,Int}
         @test lik.r == 1
     end
 
-    lik = NegativeBinomialLikelihood(1.0)
+    for args in ((normcdf,), (NormalCDFLink(),)),
+        kwargs in ((; r=2.0),)
+        lik = NegativeBinomialLikelihood(args...; kwargs...)
+        @test lik isa NegativeBinomialLikelihood{NormalCDFLink,Float64}
+        @test lik.r == 2
+    end
+
+    lik = NegativeBinomialLikelihood()
     test_interface(lik, NegativeBinomial; functor_args=(:r, :invlink))
 end
