63a4fd27845676b885819393a68919c3ed773461
Théo Galy-Fajou <theo.galyfajou@gmail.com>
2022-01-28 11:28:13 +0100

    Add inference tests

--- test/likelihoods/categorical.jl
+++ test/likelihoods/categorical.jl
@@ -1,21 +1,26 @@
 @testset "CategoricalLikelihood" begin
     Catbij = CategoricalLikelihood{true,SoftMaxLink}
-    @test CategoricalLikelihood() isa Catbij
-    @test CategoricalLikelihood(; bijective=true) isa Catbij
-    @test CategoricalLikelihood(softmax) isa Catbij
-    @test CategoricalLikelihood(SoftMaxLink()) isa Catbij
-    @test CategoricalLikelihood(softmax; bijective=true) isa Catbij
-    @test CategoricalLikelihood(SoftMaxLink(); bijective=true) isa Catbij
+    for args in ((), (softmax,), (SoftMaxLink(),)),
+        kwargs in ((), (; bijective=true), (; bijective=Val(true)))
+
+        @test CategoricalLikelihood(args...; kwargs...) isa Catbij
+        if kwargs != (; bijective=true) # Inferred does not pass with Bool keyword
+            @inferred CategoricalLikelihood(args...; kwargs...)
+        end
+    end
 
     Catnonbij = CategoricalLikelihood{false,SoftMaxLink}
-    @test CategoricalLikelihood(; bijective=false) isa Catnonbij
-    @test CategoricalLikelihood(softmax; bijective=false) isa Catnonbij
-    @test CategoricalLikelihood(SoftMaxLink(); bijective=false) isa Catnonbij
-    @test CategoricalLikelihood(softmax; bijective=Val(false)) isa Catnonbij
-    @test CategoricalLikelihood(SoftMaxLink(); bijective=Val(false)) isa Catnonbij
+    for args in ((), (softmax,), (SoftMaxLink(),)),
+        kwargs in ((; bijective=false), (; bijective=Val(false)))
 
-    lik_bijective = CategoricalLikelihood()
+        @test CategoricalLikelihood(args...; kwargs...) isa Catnonbij
+        if kwargs != (; bijective=false) # Inferred does not pass with Bool keyword
+            @inferred CategoricalLikelihood(args...; kwargs...)
+        end
+    end
+
     OUT_DIM = 4
+    lik_bijective = CategoricalLikelihood()
     test_interface(lik_bijective, Categorical, OUT_DIM)
     lik_nonbijective = CategoricalLikelihood(softmax; bijective=Val(false))
     test_interface(lik_nonbijective, Categorical, OUT_DIM)
