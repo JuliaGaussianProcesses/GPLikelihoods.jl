bd31a87932df92b40a36371dbec7a8d85d717b63
simsurace <simsurace@gmx.net>
2022-02-06 22:32:49 +0100

    Implement changes suggested by @devmotion

--- src/likelihoods/negativebinomial.jl
+++ src/likelihoods/negativebinomial.jl
@@ -1,25 +1,25 @@
 """
-    NegativeBinomialLikelihood(r::Real=1.0, l=logistic)
+    NegativeBinomialLikelihood(l=logistic; successes::Real=1)
 
-Negative binomial likelihood with number of successes `r`.
+Negative binomial likelihood with number of successes `successes`.
 
 ```math
-    p(k|f) = \\frac{\\Gamma(k+r)}{k! \\Gamma(r)} p^r (1 - p)^k
+    p(k|f) = \\frac{\\Gamma(k+successes)}{k! \\Gamma(successes)} l(f)^successes (1 - l(f))^k
 ```
-On calling, this would return a negative binomial distribution with `r` successes and probability of success equal to `l(f)`.
-Note the distinction of the role of parameter `r` to the Wikipedia definition.
+On calling, this returns a negative binomial distribution with `successes` successes and probability of success equal to `l(f)`.
+Note the distinction of the role of parameter `successes` to the parameter `r` in the Wikipedia definition.
 """
 struct NegativeBinomialLikelihood{Tl<:AbstractLink,T<:Real} <: AbstractLikelihood
-    r::T    # number of successes parameter
+    successes::T    # number of successes parameter
     invlink::Tl
 end
 
-NegativeBinomialLikelihood(l=logistic; r::Real=1) = NegativeBinomialLikelihood(r, link(l))
+NegativeBinomialLikelihood(l=logistic; successes::Real=1) = NegativeBinomialLikelihood(successes, link(l))
 
 @functor NegativeBinomialLikelihood
 
-(l::NegativeBinomialLikelihood)(f::Real) = NegativeBinomial(l.r, l.invlink(f))
+(l::NegativeBinomialLikelihood)(f::Real) = NegativeBinomial(l.successes, l.invlink(f))
 
 function (l::NegativeBinomialLikelihood)(fs::AbstractVector{<:Real})
-    return Product(NegativeBinomial.(l.r, l.invlink.(fs)))
+    return Product(NegativeBinomial.(l.successes, l.invlink.(fs)))
 end
--- test/likelihoods/negativebinomial.jl
+++ test/likelihoods/negativebinomial.jl
@@ -1,16 +1,16 @@
 @testset "NegativeBinomialLikelihood" begin
-    for args in ((), (logistic,), (LogisticLink(),)), kwargs in ((), (; r=1))
+    for args in ((), (logistic,), (LogisticLink(),)), kwargs in ((), (; successes=1))
         lik = NegativeBinomialLikelihood(args...; kwargs...)
         @test lik isa NegativeBinomialLikelihood{LogisticLink,Int}
-        @test lik.r == 1
+        @test lik.successes == 1
     end
 
-    for args in ((normcdf,), (NormalCDFLink(),)), kwargs in ((; r=2.0),)
+    for args in ((normcdf,), (NormalCDFLink(),)), kwargs in ((; successes=2.0),)
         lik = NegativeBinomialLikelihood(args...; kwargs...)
         @test lik isa NegativeBinomialLikelihood{NormalCDFLink,Float64}
-        @test lik.r == 2
+        @test lik.successes == 2
     end
 
     lik = NegativeBinomialLikelihood()
-    test_interface(lik, NegativeBinomial; functor_args=(:r, :invlink))
+    test_interface(lik, NegativeBinomial; functor_args=(:successes, :invlink))
 end
