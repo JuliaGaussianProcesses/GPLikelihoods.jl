be8fb9f89c0d6b6e483a880d6cdccadd972c074c
Théo Galy-Fajou <theo.galyfajou@gmail.com>
2022-03-22 15:14:32 +0100

    Update src/links.jl
    Co-authored-by: st-- <st--@users.noreply.github.com>

--- src/links.jl
+++ src/links.jl
@@ -39,7 +39,7 @@ This is a necessary step to work with simplices.
 For example with the [`SoftMaxLink`](@ref), to obtain a `n-1`-simplex leading to
 `n` categories for the [`CategoricalLikelihood`](@ref),
 one needs to pass `n` latent GP.
-However, by wrapping the link into a `BijectiveSimplexLink`, only `n-1` latent GP are needed. 
+However, by wrapping the link into a `BijectiveSimplexLink`, only `n-1` latent GPs are needed. 
 """
 struct BijectiveSimplexLink{L} <: AbstractLink
     link::L
