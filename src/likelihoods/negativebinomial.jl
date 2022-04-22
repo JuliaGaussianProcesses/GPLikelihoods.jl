94921fbe09aae38a289592078e795eab657e872f
Theo Galy-Fajou <theo.galyfajou@gmail.com>
2022-04-22 11:45:36 +0200

    Refactor the parametrization

--- src/likelihoods/negativebinomial.jl
+++ src/likelihoods/negativebinomial.jl
@@ -1,36 +1,47 @@
 abstract type NBParam end
 
 """
-    NegativeBinomialLikelihood{NBParam}(l=logistic/exp; kwargs...)
+    NegativeBinomialLikelihood(param::NBParam, l::Function/Link)
 
 There are many possible parametrizations for the Negative Binomial likelihood.
 The `NegativeBinomialLikelihood` has a special structure, the first type `NBParam`
-defines what parametrization is used.
+defines what parametrization is used, and contains the related parameters.
 Each `NBParam` has its own documentation:
 - [`NBParamI`](@ref): This is the definition used by Distributions.jl.
 - [`NBParamII`](@ref): This is the definition used by Wikipedia.
 - [`NBParamIII`](@ref): This is the definition based on the mean.
 
 To create a new parametrization, you need to;
-- Create a new typee from `struct MyNBParam <: NBParam end`
-- Dispatch `(l::NegativeBinomialLikelihood{MyNBParam})(f::Real)`, which return the [`NegativeBinomial`](https://juliastats.org/Distributions.jl/latest/univariate/#Distributions.NegativeBinomial) from `Distributions.jl`.
-`NegativeBinomial` follows the parametrization [`NBParamI`](@ref), i.e. the first argument is the number of successes
+- Create a new type `struct MyNBParam{T} <: NBParam; myparams::T; end`
+- Dispatch `(l::NegativeBinomialLikelihood{<:MyNBParam})(f::Real)`, which return a [`NegativeBinomial`](https://juliastats.org/Distributions.jl/latest/univariate/#Distributions.NegativeBinomial) from `Distributions.jl`.
+`NegativeBinomial` follows the parametrization of [`NBParamI`](@ref), i.e. the first argument is the number of successes
 and the second argument is the probability of success.
-- Write a constructor for `NegativeBinomialLikelihood{MyNBParam}(link; kwargs...)`
+
+## Examples
+
+```julia-repl
+julia> lik = NegativeBinomialLikelihood(NBParamI(10), logistic)
+NegativeBinomialLikelihood{NBParamI{Int64}, LogisticLink}(NBParamI{Int64}(10), LogisticLink(LogExpFunctions.logistic))
+julia> lik(2.0)
+Distributions.NegativeBinomial{Float64}(r=10.0, p=0.8807970779778824)
+
+julia> lik = NegativeBinomialLikelihood(NBParamIII(10), exp)
+NegativeBinomialLikelihood{NBParamIII{Int64}, ExpLink}(NBParamIII{Int64}(10), ExpLink(exp))
+julia> lik(2.0)
+Distributions.NegativeBinomial{Float64}(r=10.0, p=0.4249256576603398)
+```
 """
-struct NegativeBinomialLikelihood{NBParam,Tl<:AbstractLink,T} <: AbstractLikelihood
-    params::T # Likelihood parameters (depends of NBParam)
+struct NegativeBinomialLikelihood{Tp<:NBParam,Tl<:AbstractLink} <: AbstractLikelihood
+    params::Tp # Likelihood parametrization (and parameters)
     invlink::Tl
-    function NegativeBinomialLikelihood{Tparam}(params, invlink) where {Tparam}
+    function NegativeBinomialLikelihood(params::Tparam, invlink) where {Tparam}
         invlink = link(invlink)
-        return new{Tparam,typeof(invlink),typeof(params)}(params, invlink)
+        return new{Tparam,typeof(invlink)}(params, invlink)
     end
 end
 
-@deprecate NegativeBinomialLikelihood(l=logistic; successes=1) NegativeBinomialLikelihood{
-    NBParamI
-}(
-    l; successes
+@deprecate NegativeBinomialLikelihood(l=logistic; successes=1) NegativeBinomialLikelihood(
+    NBParamI(successes), l
 )
 
 function (l::NegativeBinomialLikelihood)(::Real)
@@ -39,20 +50,12 @@ function (l::NegativeBinomialLikelihood)(::Real)
     )
 end
 
-# Workaround for https://github.com/FluxML/Functors.jl/issues/40
-function Functors.functor(
-    ::Type{<:Tlik}, x
-) where {Tparam,Tlik<:NegativeBinomialLikelihood{Tparam}}
-    function reconstruct_lik(xs)
-        return NegativeBinomialLikelihood{Tparam}(xs.params, xs.invlink)
-    end
-    return (params=x.params, invlink=x.invlink), reconstruct_lik
-end
+@functor NegativeBinomialLikelihood
 
 (l::NegativeBinomialLikelihood)(fs::AbstractVector{<:Real}) = Product(map(l, fs))
 
 """
-    NBParamI
+    NBParamI(successes)
 
 Negative Binomial parametrization with `successes` the number of successes and
 `l(f)` the probability of `success`.
@@ -62,18 +65,16 @@ This corresponds to the definition used by [Distributions.jl](https://juliastats
   p(k|successes, f) = \\frac{\\Gamma(k+successes)}{k! \\Gamma(successes)} l(f)^successes (1 - l(f))^k
 ```
 """
-struct NBParamI <: NBParam end
+struct NBParamI{T} <: NBParam
+    successes::T
+end
 
-function (l::NegativeBinomialLikelihood{NBParamI})(f::Real)
+function (l::NegativeBinomialLikelihood{<:NBParamI})(f::Real)
     return NegativeBinomial(l.params.successes, l.invlink(f))
 end
 
-function NegativeBinomialLikelihood{NBParamI}(l=logistic; successes::Real=1)
-    return NegativeBinomialLikelihood{NBParamI}((; successes), l)
-end
-
 """
-    NBParamII
+    NBParamII(failures)
 
 Negative Binomial parametrization with `failures` the number of failures and
 `l(f)` the probability of `success`.
@@ -83,30 +84,26 @@ This corresponds to the definition used by [Wikipedia](https://en.wikipedia.org/
   p(k|failures, f) = \\frac{\\Gamma(k+failures)}{k! \\Gamma(failure)} l(f)^k (1 - l(f))^{failures}
 ```
 """
-struct NBParamII <: NBParam end
+struct NBParamII{T} <: NBParam
+    failures::T
+end
 
-function (l::NegativeBinomialLikelihood{NBParamII})(f::Real)
+function (l::NegativeBinomialLikelihood{<:NBParamII})(f::Real)
     return NegativeBinomial(l.params.failures, 1 - l.invlink(f))
 end
 
-function NegativeBinomialLikelihood{NBParamII}(l=logistic; failures::Real=1)
-    return NegativeBinomialLikelihood{NBParamII}((; failures), l)
-end
-
 """
-    NBParamIII
+    NBParamIII(successes)
 
 Negative Binomial parametrization with mean `μ=l(f)` and number of successes `successes`.
 See the definition given in the [Wikipedia article](https://en.wikipedia.org/wiki/Negative_binomial_distribution#Alternative_formulations)
 """
-struct NBParamIII <: NBParam end
+struct NBParamIII{T} <: NBParam
+    successes::T
+end
 
-function (l::NegativeBinomialLikelihood{NBParamIII})(f::Real)
+function (l::NegativeBinomialLikelihood{<:NBParamIII})(f::Real)
     μ = l.invlink(f)
     r = l.params.successes
     return NegativeBinomial(r, μ / (μ + r))
-end
-
-function NegativeBinomialLikelihood{NBParamIII}(l=exp; successes::Real=1)
-    return NegativeBinomialLikelihood{NBParamIII}((; successes), l)
 end
--- test/likelihoods/negativebinomial.jl
+++ test/likelihoods/negativebinomial.jl
@@ -1,30 +1,14 @@
 @testset "NegativeBinomialLikelihood" begin
-    for (nbparam, kwarg) in
-        ((NBParamI, :successes), (NBParamII, :failures), (NBParamIII, :successes))
-        @testset "$(nameof(nbparam))" begin
-            @eval begin
-                for args in ((logistic,), (LogisticLink(),)), kwargs in ((), (; $(kwarg)=1))
-                    lik = NegativeBinomialLikelihood{$(nbparam)}(args...; kwargs...)
-                    @test lik isa NegativeBinomialLikelihood{
-                        $(nbparam),LogisticLink,<:NamedTuple{<:Any,<:Tuple{Int}}
-                    }
-                end
-
-                for args in ((normcdf,), (NormalCDFLink(),)), kwargs in ((; $(kwarg)=2.0),)
-                    lik = NegativeBinomialLikelihood{$(nbparam)}(args...; kwargs...)
-                    @test lik isa NegativeBinomialLikelihood{
-                        $(nbparam),NormalCDFLink,<:NamedTuple{<:Any,<:Tuple{Float64}}
-                    }
-                end
-
-                lik = NegativeBinomialLikelihood{$(nbparam)}()
+    rs = (10, 9.5)
+    for nbparam in (NBParamI, NBParamII, NBParamIII)
+        for r in (10, 9.5) # Test both input types
+            @testset "$(nameof(nbparam)), r=$r" begin
+                lik = NegativeBinomialLikelihood(nbparam(r), logistic)
+                @test lik isa NegativeBinomialLikelihood{<:nbparam}
                 test_interface(lik, NegativeBinomial; functor_args=(:params, :invlink))
             end
         end
     end
     struct NBParamFoo <: GPLikelihoods.NBParam end
-    function NegativeBinomialLikelihood{NBParamFoo}(l=logistic; bar=1)
-        return NegativeBinomialLikelihood{NBParamFoo}((; bar), GPLikelihoods.link(l))
-    end
-    @test_throws ErrorException NegativeBinomialLikelihood{NBParamFoo}()(2.0)
+    @test_throws ErrorException NegativeBinomialLikelihood(NBParamFoo(), logistic)(2.0)
 end
