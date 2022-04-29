c23813b6ce4cc26c629a81fb8e0968b7c4ac2669
ST John <st--@users.noreply.github.com>
2022-04-29 09:59:50 +0300

    remove unknown keyword

--- docs/make.jl
+++ docs/make.jl
@@ -7,7 +7,6 @@ makedocs(;
     pages=["Home" => "index.md", "API" => "api.md"],
     repo="https://github.com/JuliaGaussianProcesses/GPLikelihoods.jl/blob/{commit}{path}#L{line}",
     authors="JuliaGaussianProcesses organization",
-    assets=String[],
     strict=true,
     checkdocs=:exports,
     #doctestfilters=JuliaGPsDocs.DOCTEST_FILTERS,
