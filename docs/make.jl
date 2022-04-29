0baf537c989a22e9fae467819a88739b3602a444
ST John <st--@users.noreply.github.com>
2022-04-29 09:39:02 +0300

    turn on checkdocs

--- docs/make.jl
+++ docs/make.jl
@@ -1,13 +1,16 @@
 using Documenter, GPLikelihoods
 
 makedocs(;
-    modules=[GPLikelihoods],
+    sitename="GPLikelihoods.jl",
     format=Documenter.HTML(),
+    modules=[GPLikelihoods],
     pages=["Home" => "index.md", "API" => "api.md"],
     repo="https://github.com/JuliaGaussianProcesses/GPLikelihoods.jl/blob/{commit}{path}#L{line}",
-    sitename="GPLikelihoods.jl",
     authors="JuliaGaussianProcesses organization",
     assets=String[],
+    strict=true,
+    checkdocs=:exports,
+    #doctestfilters=JuliaGPsDocs.DOCTEST_FILTERS,
 )
 
 deploydocs(; repo="github.com/JuliaGaussianProcesses/GPLikelihoods.jl", push_preview=true)
